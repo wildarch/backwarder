257e96ea1ee6de61584b7c680791035c2d17de68
Daan de Graaf <daandegraaf9@gmail.com>
2019-01-10 17:09:58 +0100

    Prepare for heroku.

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 const net = require('net');
+const EventEmitter = require('events');
 
+/*
 const tcpSocket = net.Socket();
 let connected = false;
 const ws = new WebSocket('ws://localhost:8888');
@@ -10,7 +12,7 @@ ws.on('message', data => {
         tcpSocket.connect(8000, "localhost");
         tcpSocket.on('end', () => {
             console.log("Closed!");
-            ws.close();
+            ws.terminate();
         });
         tcpSocket.on('data', data => {
             ws.send(data);
@@ -20,4 +22,68 @@ ws.on('message', data => {
     tcpSocket.write(data);
 });
 
+ws.on('close', () => {
+    tcpSocket.end();
+});
+*/
 
+class Tunnel extends EventEmitter {
+    constructor(socketUrl, tcpHost, tcpPort) {
+        super();
+        this.tcpSocket = new net.Socket();
+        this.connected = false;
+        this.closing = false;
+        this.webSocket = new WebSocket(socketUrl);
+
+        this.webSocket.on('message', data => {
+            if(!this.connected) {
+                this.tcpSocket.connect(tcpPort, tcpHost);
+                this.tcpSocket.on('end', () => {
+                    this.onClose();
+                });
+                this.tcpSocket.on('data', data => {
+                    this.webSocket.send(data);
+                });
+                this.connected = true;
+            }
+            this.tcpSocket.write(data);
+        });
+
+        this.webSocket.on('close', () => {
+            this.onClose();
+        });
+    }
+
+    close() {
+        this.tcpSocket.end();
+        this.webSocket.terminate();
+        this.connected = false;
+    }
+
+    onClose() {
+        if(!this.closing) {
+            this.closing = true;
+            // Just to be sure
+            this.close();
+            this.emit('close');
+        }
+    }
+}
+
+let tunnelCounter = 0;
+
+function onTunnelClose() {
+    tunnelCounter++;
+    console.log("Opening tunnel " + tunnelCounter);
+
+    const tunnel = new Tunnel("ws://localhost:8888", "localhost", 8000);
+    tunnel.on('close', () => {
+        onTunnelClose();
+    });
+}
+
+
+// Make a pool
+for(let i = 0; i < 10; i++) {
+    onTunnelClose();
+}
--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
 
+const port = process.env.PORT || 8888;
+
 let freeSockets = [];
+
+let socketCounter = 0;
 
 app.ws('/', (ws, req) => {
     freeSockets.push(ws);
-    console.log("New backconnect socket added")
+    socketCounter++;
+    console.log("Added backconnect socket " + socketCounter);
 });
 
-app.get('/', (req, res, next) => {
+app.use((req, res) => {
     const ws = freeSockets.pop();
+
+    // If no free backconnect socket is available, report temporarily unavailable.
     if(!ws) {
         res.status(503);
         res.end();
         return;
     }
 
-    ws.send(Buffer.from("GET " + req.originalUrl + " HTTP/1.1\r\n"));
+    // Reconstruct HTTP Header
+    ws.send(Buffer.from(req.method + " " + req.originalUrl + " HTTP/1.1\r\n"));
     for(let header in req.headers) {
         if(req.headers.hasOwnProperty(header)) {
             ws.send(Buffer.from(header + ": " + req.headers[header] + "\r\n"));
@@ -25,12 +33,15 @@ app.get('/', (req, res, next) => {
     }
     ws.send(Buffer.from("\r\n"));
 
+    // Forward data
     req.on('data', chunk => {
         ws.send(chunk);
     });
     ws.on('message', chunk => {
         res.write(chunk);
     });
+
+    // Synchronize close
     ws.on('close', () => {
         res.end();
     });
@@ -39,4 +50,4 @@ app.get('/', (req, res, next) => {
     });
 });
 
-app.listen(8888);
+app.listen(port);
