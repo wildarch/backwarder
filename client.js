0e8ab47417734a3c93018338d8d2da982db3cf97
Daan de Graaf <daandegraaf9@gmail.com>
2019-02-10 13:12:40 +0100

    More logging.

--- client.js
+++ client.js
@@ -2,63 +2,62 @@ const WebSocket = require('ws');
 const net = require('net');
 const EventEmitter = require('events');
 
+let tunnel_host = process.argv[2];
+
 class Tunnel extends EventEmitter {
-    constructor(socketUrl, tcpHost, tcpPort) {
-        super();
-        this.tcpSocket = new net.Socket();
-        this.connected = false;
-        this.closing = false;
-        this.webSocket = new WebSocket(socketUrl);
+  constructor(socketUrl, tcpHost, tcpPort) {
+    super();
+    this.tcpSocket = new net.Socket();
+    this.connected = false;
+    this.closing = false;
+    this.webSocket = new WebSocket(socketUrl);
 
-        this.webSocket.on('message', data => {
-            if(!this.connected) {
-                this.tcpSocket.connect(tcpPort, tcpHost);
-                this.tcpSocket.on('end', () => {
-                    this.onClose();
-                });
-                this.tcpSocket.on('data', data => {
-                    this.webSocket.send(data);
-                });
-                this.connected = true;
-            }
-            this.tcpSocket.write(data);
+    this.webSocket.on('message', data => {
+      if (!this.connected) {
+        this.tcpSocket.connect(tcpPort, tcpHost);
+        this.tcpSocket.on('end', () => {
+          console.log("TCP socket end");
+          this.onClose();
         });
+        this.tcpSocket.on('data', data => { this.webSocket.send(data); });
+        this.connected = true;
+      }
+      this.tcpSocket.write(data);
+    });
 
-        this.webSocket.on('close', () => {
-            this.onClose();
-        });
-    }
+    this.webSocket.on('close', () => {
+      console.log("Websocket end");
+      this.onClose();
+    });
+  }
 
-    close() {
-        this.tcpSocket.end();
-        this.webSocket.terminate();
-        this.connected = false;
-    }
+  close() {
+    this.tcpSocket.end();
+    this.webSocket.terminate();
+    this.connected = false;
+  }
 
-    onClose() {
-        if(!this.closing) {
-            this.closing = true;
-            // Just to be sure
-            this.close();
-            this.emit('close');
-        }
+  onClose() {
+    if (!this.closing) {
+      this.closing = true;
+      // Just to be sure
+      this.close();
+      this.emit('close');
     }
+  }
 }
 
 let tunnelCounter = 0;
 
 function onTunnelClose() {
-    tunnelCounter++;
-    console.log("Opening tunnel " + tunnelCounter);
+  tunnelCounter++;
+  console.log("Opening tunnel " + tunnelCounter);
 
-    const tunnel = new Tunnel("ws:/localhost:8888", "localhost", 8000);
-    tunnel.on('close', () => {
-        onTunnelClose();
-    });
+  const tunnel = new Tunnel(tunnel_host, "localhost", 8000);
+  tunnel.on('close', () => { onTunnelClose(); });
 }
 
-
 // Make a pool
-for(let i = 0; i < 10; i++) {
-    onTunnelClose();
+for (let i = 0; i < 10; i++) {
+  onTunnelClose();
 }
